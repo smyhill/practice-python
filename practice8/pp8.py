7dbc9f82a895f702e80f79dd85b127ff534c6ebc
Simon Myhill <myhillsimon96@gmail.com>
2021-09-28 16:57:45 -0400

    finished pp8

--- practice8/pp8.py
+++ practice8/pp8.py
@@ -1,10 +1,48 @@
 # Make a two-player Rock-Paper-Scissors game. 
 # (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
 
-p1 = str(input('Please enter r for rock, p for paper or s for scissors: '))
-while p1 != 'r' or p1 != 'p' or p1 != 's':
-    print("please enter a valid option")
-    p1 = input('Please enter r for rock, p for paper or s for scissors: ')
-    if p1 == 'r' or p1 == 'p' or p1 == 's':
-        break
+def rpc():
+    p1 = str(input('Player 1: please enter r for rock, p for paper or s for scissors: '))
+    p2 = str(input('Player 2: please enter r for rock, p for paper or s for scissors: '))
+
+    while p1 == 'r':
+        if p2 == 'r':
+            print("Both players chose Rock: Tie!")
+            break
+        elif p2 == 'p':
+            print("Paper Beats Rock: Player 2 Wins!")
+            break
+        else:
+            print("Rock Beats Scissors: Player 1 Wins!")
+            break    
+
+    while p1 == 'p':
+        if p2 == 'r':
+            print("Paper Beats Rock: Player 1 Wins!")
+            break
+        elif p2 == 'p':
+            print("Both Players Chose Paper: Tie!")
+            break
+        else:
+            print("Scissors Beats Paper: Player 2 Wins!")
+            break  
+        
+    while p1 == 's':
+        if p2 == 'r':
+            print("Rock Beats Scissors: Player 2 Wins!")
+            break
+        elif p2 == 'p':
+            print("Scissors Beats Paper: Player 1 Wins!")
+            break
+        else:
+            print(" Both Players Chose Scissors: Tie!")
+            break  
+    print("Play Again?")
+rpc()
+
+yn = input('Type "Y" to play again or "N" to quit: ')
+while yn == "y":
+    rpc()
+    yn = input('Type "Y" to play again or "N" to quit: ')
+
 
