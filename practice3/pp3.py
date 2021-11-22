ec54140a5421c1b710b3afa532fe011ded72d91b
Simon Myhill <myhillsimon96@gmail.com>
2021-11-22 16:39:30 -0500

    Update pp3.py

--- practice3/pp3.py
+++ practice3/pp3.py
@@ -4,9 +4,9 @@
 # Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
 
 list = [1, 1, 2, 2, 3, 5, 9, 15, 27, 43, 66, 79]
-less_than_5_list = []
+less_than_7_list = []
 threshold = input("Please enter a threshold: ")
 for i in list:
     if i < int(threshold):
-        less_than_5_list.append(i)
-print(less_than_5_list)
+        less_than_7_list.append(i)
+print(less_than_7_list)
